0e93ae3693843a6cc3745f3cdbb1c8b533c7ce7d
dirkdirk <dirkdirk@users.noreply.github.com>
2021-06-20 09:14:47 -0700

    101 and 102

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
-import { NgModule } from '@angular/core'
+import { NgModule }      from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
-import { FormsModule } from '@angular/forms'
+import { FormsModule }   from '@angular/forms'
 
-import { AppComponent } from './app.component'
-import { HeaderComponent } from './header/header.component'
-import { RecipesComponent } from './recipes/recipes.component'
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component'
+import { AppComponent }          from './app.component'
+import { HeaderComponent }       from './header/header.component'
+import { RecipesComponent }      from './recipes/recipes.component'
+import { RecipeListComponent }   from './recipes/recipe-list/recipe-list.component'
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component'
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component'
+import { RecipeItemComponent }   from './recipes/recipe-list/recipe-item/recipe-item.component'
 import { ShoppingListComponent } from './shopping-list/shopping-list.component'
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component'
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { DropdownDirective }     from './directives/dropdown.directive'
 
 @NgModule({
     declarations: [
@@ -20,7 +21,8 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
         RecipeDetailComponent,
         RecipeItemComponent,
         ShoppingListComponent,
-        ShoppingEditComponent
+        ShoppingEditComponent,
+        DropdownDirective
     ],
     imports: [
         BrowserModule,
