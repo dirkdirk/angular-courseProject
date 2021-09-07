0f4e3286ac107ccd1d44c480afec6868810ba552
dirkdirk <dirkdirk@users.noreply.github.com>
2021-09-07 13:27:30 -0700

    x

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,8 @@
-import { Component, Input } from '@angular/core'
+import { Component } from '@angular/core'
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-    title = 'courseProject'
-
-    menuSelected = 'recipes'
-
-    @Input() menuItem: string
-
-    menuItemClicked(menuItem: string) {
-        // console.log(`menuItem: ${menuItem}`)
-        this.menuSelected = menuItem
-    }
-
-}
+export class AppComponent { }
